9972cf8d681455000577573eb691722507f96ac5
huajiaAAA333 <2509802388@qq.com>
2020-09-22 20:07:18 +0800

    gkd

--- controllers/default.go
+++ controllers/default.go
@@ -10,6 +10,6 @@ type MainController struct {
 
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["Email"] = "2509802388@qq.com"
 	c.TplName = "index.tpl"
 }
